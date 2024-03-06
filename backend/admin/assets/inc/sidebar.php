05bd709a6ec29b91012139cc5201952564e14205
Rutwij Vaze <121rutwij4002@sjcem.edu.in>
2024-03-06 15:26:19 +0530

    Initial commit

---
+++ backend/admin/assets/inc/sidebar.php
@@ -0,0 +1,139 @@
+<div class="left-side-menu">
+
+                <div class="slimscroll-menu">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+
+                        <ul class="metismenu" id="side-menu">
+
+                            <li class="menu-title">Navigation</li>
+
+                            <li>
+                                <a href="his_admin_dashboard.php">
+                                    <i class="fe-airplay"></i>
+                                    <span> Dashboard </span>
+                                </a>
+                                
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fab fa-accessible-icon "></i>
+                                    <span> Patients </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_register_patient.php">Register Patient</a>
+                                    </li>
+                            
+                                    <li>
+                                        <a href="his_admin_manage_patient.php">Manage Patients</a>
+                                    </li>
+                                    <hr>
+                                    <li>
+                                        <a href="his_admin_discharge_patient.php">Discharge Patients</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_patient_transfer.php">Patient Transfers</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-doctor"></i>
+                                    <span> Employees </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_add_employee.php">Add Employee</a>
+                                    </li>
+                                    
+                                    <li>
+                                        <a href="his_admin_manage_employee.php">Manage Employees</a>
+                                    </li>
+                                 
+                                </ul>
+                            </li>
+
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="mdi mdi-pill"></i>
+                                    <span> Pharmacy </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                                                    <li>
+                                        <a href="his_admin_add_presc.php">Add Prescriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_view_presc.php">View Prescriptions</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_manage_presc.php">Manage Prescriptions</a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                           
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class=" fas fa-funnel-dollar "></i>
+                                    <span> Inventory </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                   
+                                    <li>
+                                        <a href="his_admin_pharm_inventory.php">Pharmaceuticals</a>
+                                    </li>
+
+                                 
+                                    
+                                </ul>
+                            </li>
+                
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fe-share"></i>
+                                    <span> Reporting </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_inpatient_records.php">InPatient Records</a>
+                                    </li>
+                                    <li>
+                                        <a href="his_admin_outpatient_records.php">OutPatient Records</a>
+                                    </li>
+                          
+                        
+                        
+                            <li>
+                                <a href="javascript: void(0);">
+                                    <i class="fas fa-lock"></i>
+                                    <span> Password Resets </span>
+                                    <span class="menu-arrow"></span>
+                                </a>
+                                <ul class="nav-second-level" aria-expanded="false">
+                                    <li>
+                                        <a href="his_admin_manage_password_resets.php">Manage</a>
+                                    </li>
+                                                                        
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                    <!-- End Sidebar -->
+
+                    <div class="clearfix"></div>
+
+                </div>
+                <!-- Sidebar -left -->
+
+            </div>
---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta Tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <!-- Page Title -->
+    <title>Hospital Management System</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="assets/images/logo/favicon.png" type="image/x-icon">
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/animate-3.7.0.css">
+    <link rel="stylesheet" href="assets/css/font-awesome-4.7.0.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap-4.1.3.min.css">
+    <link rel="stylesheet" href="assets/css/owl-carousel.min.css">
+    <link rel="stylesheet" href="assets/css/linearicons.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- Custom Styles -->
+    <style>
+        body {
+            background-image: url("https://ibb.co/6Wfh4zq"); 
+            background-size: cover; 
+            background-position: center; 
+        }
+    </style>
+</head>
+<body>
+
+
+    <!-- Header Area Starts -->
+    <header class="header-area">
+        <div id="header" id="home">
+            <div class="container">
+                <div class="row align-items-center justify-content-between d-flex">
+                <div id="logo">
+                    <a href="index.php"></a>
+                </div>
+                <nav id="nav-menu-container">
+                    <ul class="nav-menu">
+                        <li class="menu-active"><a href="index.php">Home</a></li>
+                        <li><a href="backend/doc/index.php">Doctor's Login</a></li>
+                        <li><a href="backend/admin/index.php">Administrator Login</a></li>
+                    </ul>
+                </nav><!-- #nav-menu-container -->                  
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- Header Area End -->
+
+    <!-- Banner Area Starts -->
+    <section class="banner-area" style="background-image: url('./assets/images/banner-bg.jpeg');background-repeat: no-repeat;
+  background-position: center;"> 
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                    <h4>Caring for better life</h4>
+                    <h1>Leading the way in medical excellence</h1>
+                    <p>Welcome to HMS's MIS homepage! Experience seamless healthcare management with our cutting-edge system. From electronic health records to efficient resource management, we're dedicated to enhancing patient care. Join us in revolutionizing healthcare today!</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Javascript -->
+    <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
+    <script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
+    <script src="assets/js/vendor/wow.min.js"></script>
+    <script src="assets/js/vendor/owl-carousel.min.js"></script>
+    <script src="assets/js/vendor/jquery.datetimepicker.full.min.js"></script>
+    <script src="assets/js/vendor/jquery.nice-select.min.js"></script>
+    <script src="assets/js/vendor/superfish.min.js"></script>
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
